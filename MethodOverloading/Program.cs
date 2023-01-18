3570fbc9e99560649807dbfdf511cb00e9078ed6
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-01-18 17:06:37 -0600

    completed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,53 @@
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+       
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isCheck)
+        {
+            var sum = num1+ num2;
+            
+
+            if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if(isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if(isCheck == true && sum < 1)
+            {
+                return $"{sum} cents";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = Add(27, 50);
+
+            var decimalAnswer = Add(27.0m, 50.1m);
+
+            var otherAnswer = Add(5, 28, true);
+
+            Console.WriteLine($"int add: {answer} and decimal add: {decimalAnswer}");
+            Console.WriteLine(otherAnswer);
+
+
         }
     }
 }
